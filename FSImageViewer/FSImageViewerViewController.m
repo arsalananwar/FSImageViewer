17fa69cf57deb178da3d6281d8035aaa48912ee7
M. Arsalan Anwar <arsalananwar@gmail.com>
2016-06-24 13:31:43 +0500

    change the Bar button item location to Left.

--- FSImageViewer/FSImageViewerViewController.m
+++ FSImageViewer/FSImageViewerViewController.m
@@ -138,12 +138,12 @@
         UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:[self localizedStringForKey:@"done" withDefault:@"Done"] style:UIBarButtonItemStyleDone target:self action:@selector(done:)];
         self.navigationItem.rightBarButtonItem = doneButton;
         if (!_sharingDisabled) {
-            self.navigationItem.leftBarButtonItems = [NSArray arrayWithObjects:infoBarButtonItem,shareButton,nil];
+            self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:infoBarButtonItem,shareButton,nil];
         }
     }
     else {
         if (!_sharingDisabled) {
-            self.navigationItem.leftBarButtonItems = [NSArray arrayWithObjects:infoBarButtonItem,shareButton,nil];
+            self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:infoBarButtonItem,shareButton,nil];
         }
     }
 
